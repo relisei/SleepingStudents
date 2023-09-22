35bdb7f63c631c94d808191e9fda55f4fdb53ed4
ramiro elisei <ramiroelisei@gmail.com>
2023-09-22 13:34:17 +0200

    Add student object

---
+++ index.js
@@ -0,0 +1,11 @@
+const studentA = {
+  name: "Pepa",
+  surname: "Pig",
+  lastNightSleepHours: 4,
+  mark: 4,
+  introduceYourself() {
+    console.log("Hola");
+  },
+};
+
+console.log(studentA.name);
